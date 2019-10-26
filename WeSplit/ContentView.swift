35742632a4b95fbbcf88f331f24d532ef41c0295
Levit Osei Kanner <lkanner21@gmail.com>
2019-10-26 12:45:40 +0000

    WeSplit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,13 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+    //Properties
+    @State var numberOfPeople = 0
+    @State var tipPercentage = 0
+    @State var checkAmount = ""
+    let tipSizes = [10,15,20,25,0]
+    
+    //Body definition
+    var body: some View{
+        NavigationView{
+            Form{
+                Section{
+                    TextField("CheckAmount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0)people")
+                        }
+                    }
+                    
+                }
+                Section{
+                    Text("$\(checkAmount)")
+                }
+                .navigationBarTitle("WeSplit")
+
+              
+            }
+        }
+        }
     }
-}
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+/*
+ What I have learnt today in SwiftUI ...
+ 1. Text views
+ 2. Forms
+ 3. Groups to break the child limit on items
+ 4. Section to group items in a form
+ 5. Navigation View
+ 6. Buttons
+ 7. Property wrappers -- @states allows mutation of a variable in a struct
+ 8. Two-binding(Read and Write a variable) with $
+ */
